3f87ed99ce305813c0a7b679b4056ad648ab3a90
Timothy C. Arland <tcarland@gmail.com>
2017-08-03 09:54:03 -0700

    sync version updates

--- src/test/resources/build-test-jar.sh
+++ src/test/resources/build-test-jar.sh
@@ -3,6 +3,6 @@
 #  Custom jar building for the test classes
 #
 
-jar -cvf target/spark-hive-tools-test-0.1.7.jar -C target/classes . -C target/test-classes .
+( jar -cvf target/spark-hive-tools-test-0.1.8.jar -C target/classes . -C target/test-classes . )
 
 
