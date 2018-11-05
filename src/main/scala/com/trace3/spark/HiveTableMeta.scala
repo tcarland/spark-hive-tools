4ffec35a7f574179e30826069fe0c36cd2eb3f82
tca <tcarland@gmail.com>
2018-11-05 07:06:30 -0800

    add meta restore function

--- src/main/scala/com/trace3/spark/HiveTableMeta.scala
+++ src/main/scala/com/trace3/spark/HiveTableMeta.scala
@@ -5,6 +5,7 @@
 package com.trace3.spark
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.FileUtil
 import org.apache.hadoop.fs.Path
@@ -21,9 +22,10 @@ object HiveTableMeta {
 
   val usage : String =
     """
-      | ==>  Usage: HiveTableMeta <dbname> <filename>
-      | ==>      dbname    = Name of schema or database to dump.
+      | ==>  Usage: HiveTableMeta <action> <filename> <dbname>
+      | ==>      action    = save|restore
       | ==>     filename   = Name of output file of create statements.
+      | ==>      dbname    = Name of schema or database to dump.
     """.stripMargin
 
 
@@ -57,14 +59,31 @@ object HiveTableMeta {
   }
 
 
+  def RestoreTableMeta ( spark: SparkSession, inFile: String ) : Unit = {
+    import spark.implicits._
+
+    val schema = StructType(
+      StructField("NAME", StringType, true) ::
+      StructField("STMT", StringType, true) :: Nil
+    )
+
+    spark.read
+      .schema(schema)
+      .csv(inFile)
+      .collect
+      .foreach( row => spark.sql(row(1).toString) )
+  }
+
+
   def main ( args: Array[String] ) : Unit = {
     if ( args.length < 2 ) {
       System.err.println(usage)
       System.exit(1)
     }
 
-    val dbname = args(0)
-    val output = args(1)
+    val action  = args(0)
+    val csvfile = args(1)
+    val dbname  = if ( args.length == 3 ) args(2) else s""
 
     val spark = SparkSession
       .builder()
@@ -73,7 +92,10 @@ object HiveTableMeta {
       .getOrCreate
     spark.sparkContext.setLogLevel("WARN")
 
-    HiveTableMeta.SaveTableMeta(spark, dbname, output)
+    if ( action.equalsIgnoreCase("save") )
+      HiveTableMeta.SaveTableMeta(spark, dbname, csvfile)
+    else if ( action.equalsIgnoreCase("restore") )
+      HiveTableMeta.RestoreTableMeta(spark, csvfile)
 
     println(" => Finished.")
     spark.stop
