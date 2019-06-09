69343a5c81cf435abb5c8fa3dbc44e86c8f0945d
tca <tcarland@gmail.com>
2019-06-08 19:22:17 -0700

    sync config patterns

--- bin/dbvalidate.sh
+++ bin/dbvalidate.sh
@@ -3,13 +3,13 @@
 # spark-submit for DbValidate
 #
 
-APP="com.trace3.spark.DbValidate"
-
 cwd=$(dirname "$(readlink -f "$0")")
 . $cwd/hive-tools-config.sh
 
+APP_CLASS="com.trace3.spark.DbValidate"
+
 spark-submit --master yarn \
   --deploy-mode client \
-  --class $APP \
+  --class $APP_CLASS \
   $APP_JAR \
   $@
--- bin/hive-tools-config.sh
+++ bin/hive-tools-config.sh
@@ -1,9 +1,9 @@
 # spark-hive-tools runtime configuration
 #
 
-export SPARK_HIVETOOLS_VERSION="0.2.7"
-export SPARK_VERSION="2.4.2"
-export KAFKA_VERSION="0-10_2.11"
+export SPARK_HIVETOOLS_VERSION="0.2.8"
+export SPARK_VERSION="2.4.3"
+export KAFKA_VERSION="0-10_2.12"
 
 APP_JAR="target/spark-hive-tools-$SPARK_HIVETOOLS_VERSION-jar-with-dependencies.jar"
 KAFKA_JAR="$HOME/.m2/repository/org/apache/spark/spark-sql-kafka-${KAFKA_VERSION}/\
