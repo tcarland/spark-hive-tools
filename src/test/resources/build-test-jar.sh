09a77d166c9b2d5c6d21bdca0c53e40ca0a5a70c
tca <tcarland@gmail.com>
2019-06-10 08:04:53 -0700

    sync test versions

--- src/test/resources/build-test-jar.sh
+++ src/test/resources/build-test-jar.sh
@@ -4,4 +4,7 @@
 #
 
 echo "Building Test Jar..."
-( jar -cvf target/spark-hive-tools-test-0.2.7.jar -C target/classes . -C target/test-classes . )
+
+( jar -cvf target/spark-hive-tools-test-0.2.8.jar -C target/classes . -C target/test-classes . )
+
+exit $?
