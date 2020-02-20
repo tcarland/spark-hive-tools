afcf3c3e91835de45f7fd1bbe7be21ee55225667
tca <tcarland@gmail.com>
2020-02-20 08:43:39 -0800

    bump spark version to latest tested, spark 2.4.4

--- bin/hive-tools-config.sh
+++ bin/hive-tools-config.sh
@@ -2,7 +2,7 @@
 #
 
 export SPARK_HIVETOOLS_VERSION="0.2.8"
-export SPARK_VERSION="2.4.3"
+export SPARK_VERSION="2.4.4"
 export KAFKA_VERSION="0-10_2.12"
 
 APP_JAR="target/spark-hive-tools-$SPARK_HIVETOOLS_VERSION-jar-with-dependencies.jar"
