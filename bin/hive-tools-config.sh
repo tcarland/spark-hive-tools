b9b417ad2646043cbee49c450be16235ccfd2b2a
Timothy C. Arland <tcarland@users.noreply.github.com>
2024-03-27 18:35:08 -0700

    version updates (#26)

--- bin/hive-tools-config.sh
+++ bin/hive-tools-config.sh
@@ -1,10 +1,7 @@
 # spark-hive-tools runtime configuration
 #
 
-export SPARK_HIVETOOLS_VERSION="0.6.2"
-export SPARK_VERSION="3.3.2"
-export KAFKA_VERSION="0-10_2.12"
+export SPARK_HIVETOOLS_VERSION="0.6.3"
+export SPARK_VERSION="3.5.1"
 
 APP_JAR="target/spark-hive-tools-$SPARK_HIVETOOLS_VERSION-jar-with-dependencies.jar"
-KAFKA_JAR="$HOME/.m2/repository/org/apache/spark/spark-sql-kafka-${KAFKA_VERSION}/\
-${SPARK_VERSION}/spark-sql-kafka-${KAFKA_VERSION}-${SPARK_VERSION}.jar"
